5213da54781d6fb459181811ec71e3046a4ce567
moki <morozov.kirill.moki@gmail.com>
2023-04-07 13:31:04 +0400

    feat(api/compose): parameters validation

--- src/api/compose.ts
+++ src/api/compose.ts
@@ -1,4 +1,17 @@
-function compose() {}
+export type ComposeParameters = {
+    skeleton: string;
+    xlf: string;
+};
+
+function compose(parameters: ComposeParameters) {
+    if (!validate(parameters)) {
+        throw new Error('invalid parameters');
+    }
+}
+
+function validate(parameters: ComposeParameters) {
+    return parameters.skeleton !== undefined && parameters.xlf !== undefined;
+}
 
 export {compose};
 export default {compose};
