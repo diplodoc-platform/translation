b1b3784a82a2f6c7bc268a93c233dd5a9313dd3d
moki <morozov.kirill.moki@gmail.com>
2023-04-07 13:31:39 +0400

    test:(api/compose): parameters validation

--- src/api/compose.test.ts
+++ src/api/compose.test.ts
@@ -1,7 +1,39 @@
-import {compose} from './compose';
+import {compose, ComposeParameters} from './compose';
 
-describe('compose', () => {
-    test('smoke', () => {
-        compose();
+describe('smoke', () => {
+    it('works', () => {
+        const parameters = {
+            skeleton: '',
+            xlf: '',
+        };
+
+        compose(parameters);
+    });
+});
+
+describe('validates parameters', () => {
+    it('works with valid parameters', () => {
+        const parameters = {
+            skeleton: '',
+            xlf: '',
+        };
+
+        compose(parameters);
+    });
+
+    it('throws on invalid parameters', () => {
+        const invalidSkeleton = {
+            xlf: '',
+        };
+
+        const invalidXLF = {
+            skeleton: '',
+        };
+
+        const invalidBoth = {};
+
+        expect(() => compose(invalidSkeleton as ComposeParameters)).toThrow();
+        expect(() => compose(invalidXLF as ComposeParameters)).toThrow();
+        expect(() => compose(invalidBoth as ComposeParameters)).toThrow();
     });
 });
