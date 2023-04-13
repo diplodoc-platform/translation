a4db967efe516f291cbe43f629a9af1ed01a82f4
moki <morozov.kirill.moki@gmail.com>
2023-04-13 22:47:15 +0400

    feat(api/compose): provide trans-units into compose

--- src/api/compose.ts
+++ src/api/compose.ts
@@ -1,16 +1,26 @@
+import xlf from 'src/xlf';
+
 export type ComposeParameters = {
     skeleton: string;
     xlf: string;
 };
 
 function compose(parameters: ComposeParameters) {
-    if (!validate(parameters)) {
+    if (!validParameters(parameters)) {
         throw new Error('invalid parameters');
     }
+
+    const translations = xlf.parser.translationUnits(parameters.xlf);
 }
 
-function validate(parameters: ComposeParameters) {
-    return parameters.skeleton !== undefined && parameters.xlf !== undefined;
+function validParameters(parameters: ComposeParameters) {
+    const conditions = [
+        parameters.skeleton !== undefined,
+        parameters.xlf !== undefined,
+        xlf.parser.validParameters(parameters.xlf),
+    ];
+
+    return conditions.reduce((a, v) => a && v, true);
 }
 
 export {compose};
