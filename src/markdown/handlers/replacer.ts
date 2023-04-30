188a8c43aaeecb1aa1bdd88bf4eaa84a2248270b
moki <morozov.kirill.moki@gmail.com>
2023-04-30 18:03:03 +0400

    feat(markdown/renderer): handle liquid

--- src/markdown/handlers/replacer.ts
+++ src/markdown/handlers/replacer.ts
@@ -1,30 +1,43 @@
 import {replaceAfter} from 'src/string';
+import {tokenize, LiquidTokenType} from 'src/liquid';
 
 import {MarkdownHandlersState} from './index';
 
 function replacer(content: string, state: MarkdownHandlersState) {
-    let replaced = content;
-    let success = true;
-    let cursor = 0;
+    let result = '';
 
     const translations = state.markdown.translations;
 
-    while (success) {
-        const id = state.markdown.id;
-        const hash = `%%%${id}%%%`;
-
-        const translation = translations.get(String(id));
-        if (!translation?.length) {
-            return replaced;
+    for (const token of tokenize(content)) {
+        if (token.type === LiquidTokenType.Liquid) {
+            result += token.lexemme;
+            continue;
         }
 
-        ({replaced, cursor, success} = replaceAfter(replaced, hash, translation, cursor));
-        if (success) {
-            state.markdown.id++;
+        let replaced = token.lexemme;
+        let success = true;
+        let cursor = 0;
+
+        while (success) {
+            const id = state.markdown.id;
+            const hash = `%%%${id}%%%`;
+
+            const translation = translations.get(String(id));
+            if (!translation?.length) {
+                success = false;
+                continue;
+            }
+
+            ({replaced, cursor, success} = replaceAfter(replaced, hash, translation, cursor));
+            if (success) {
+                state.markdown.id++;
+            }
         }
+
+        result += replaced;
     }
 
-    return replaced;
+    return result;
 }
 
 export {replacer};
