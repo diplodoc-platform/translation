02ed069bee52d5e4911f1e6852681835e53876d6
moki <morozov.kirill.moki@gmail.com>
2023-05-02 19:56:48 +0400

    test: markdown with meta data fixture

--- src/__fixtures__/index.ts
+++ src/__fixtures__/index.ts
@@ -206,6 +206,46 @@ const translationsWithLoops = new Map<string, string>([
     ['5', 'после однострочного цикла'],
 ]);
 
+const markdownWithMeta = `---
+title: Создание виртуальной машины Windows
+description: Cоздайте виртуальную машину Windows в облаке с помощью сервиса {{ compute-short-name }}. Вы можете подключиться к виртуальной машине Windows используя Remote Dektop Protocol (RDP), встроенный в образ ОС. Перед подключением к виртуальной машине Windows убедитесь, что NLA включен в настройках вашего компьютера.
+
+key: >
+  Sammy Sosa
+
+  Let's Goooo
+
+seq:
+  - windows
+  - windows vm
+  - windows вм
+  - windows виртуальная машина
+  - виртуальная машина
+  - вм
+
+seq_of_mappings:
+  -
+    name: Mark
+    age: 13
+  -
+    name: Noam
+    age: 99
+
+# comment
+
+object_inline: {name: John Smith, age: 33}
+array_inline: [milk, pumpkin pie, eggs, juice]
+seq_of_seq:
+  - [Mark McGwire, 65, 0.278]
+  - [Sammy Sosa  , 63, 0.288]
+nesting:
+  - { children: [{name: bob}, {name: marry}]}
+---
+# Мета
+
+Комментарии и метаданные — это элементы разметки, которые не отображаются в собранном файле. Они используются, чтобы хранить в исходном тексте информацию для SEO или авторов документа.
+`;
+
 export {
     markdown,
     skeleton,
@@ -225,6 +265,7 @@ export {
     markdownWithFunctions,
     skeletonWithFunctions,
     translationsWithFunctions,
+    markdownWithMeta,
 };
 
 export default {
@@ -246,4 +287,5 @@ export default {
     markdownWithFunctions,
     skeletonWithFunctions,
     translationsWithFunctions,
+    markdownWithMeta,
 };
