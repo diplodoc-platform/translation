dfd7f3b4e3c6113b157061c6dfa598e10aacd1dd
moki <morozov.kirill.moki@gmail.com>
2023-05-25 23:40:55 +0400

    feat(markdown/renderer): render notes

--- src/markdown/renderer.ts
+++ src/markdown/renderer.ts
@@ -8,6 +8,7 @@ import {
 // configure with diplodoc plugins
 // @ts-ignore
 import meta from 'markdown-it-meta';
+import notes from '@doc-tools/transform/lib/plugins/notes';
 
 import markdownHandlers, {MarkdownHandlersState} from './handlers';
 import hooks, {HooksParameters} from './hooks';
@@ -17,6 +18,10 @@ export type MarkdownRendererState = MarkdownHandlersState;
 export type RenderParameters = {
     skeleton: string;
     translations: Map<string, string>;
+} & DiplodocParameters;
+
+export type DiplodocParameters = {
+    lang?: string;
 };
 
 function render(parameters: RenderParameters) {
@@ -33,22 +38,43 @@ function render(parameters: RenderParameters) {
         initState,
         hooks: markdownHooks.hooks,
     };
+    const diplodocOptions = {
+        lang: parameters.lang ?? 'ru',
+    };
 
     const env: MarkdownRendererEnv = {
         source: parameters.skeleton.split('\n'),
     };
 
     md.use(mdRenderer, mdOptions);
+
+    // diplodoc plugins
     md.use(meta);
+    md.use(notes, diplodocOptions);
 
     return md.render(parameters.skeleton, env);
 }
 
 function validParameters(parameters: RenderParameters) {
-    const conditions = [parameters.skeleton !== undefined, parameters.translations !== undefined];
+    const {skeleton, translations} = parameters;
+
+    const skeletonCondition = skeleton !== undefined;
+    const translationsCondition = translations !== undefined;
+
+    const conditions = [
+        skeletonCondition,
+        translationsCondition,
+        validDiplodocParameters(parameters),
+    ];
 
     return conditions.reduce((a, v) => a && v, true);
 }
 
-export {render, validParameters};
-export default {render, validParameters};
+function validDiplodocParameters(parameters: DiplodocParameters) {
+    const {lang} = parameters;
+
+    return lang === undefined || lang === 'ru' || lang === 'en';
+}
+
+export {render, validParameters, validDiplodocParameters};
+export default {render, validParameters, validDiplodocParameters};
