64bcb5c3f27e8905741f9a95e4fba01c25ec84cf
moki <morozov.kirill.moki@gmail.com>
2023-06-09 15:09:30 +0400

    test(markdown/renderer): sup

--- src/__fixtures__/sup.ts
+++ src/__fixtures__/sup.ts
@@ -4,5 +4,23 @@ const markdown = `\
 [1^ая^ ссылка в параграфе](file.md)
 `;
 
-export {markdown};
-export default {markdown};
+const skeleton = `\
+%%%1%%%^%%%2%%%^ %%%3%%%
+%%%4%%%^%%%5%%%^ %%%6%%%
+[%%%7%%%^%%%8%%%^ %%%9%%%](file.md)
+`;
+
+const translations = new Map<string, string>([
+    ['1', 'paragraph of text 1'],
+    ['2', 'st'],
+    ['3', 'line'],
+    ['4', 'this is 2'],
+    ['5', 'nd'],
+    ['6', 'line'],
+    ['7', '1'],
+    ['8', 'st'],
+    ['9', 'link inside the paragraph'],
+]);
+
+export {markdown, skeleton, translations};
+export default {markdown, skeleton, translations};
