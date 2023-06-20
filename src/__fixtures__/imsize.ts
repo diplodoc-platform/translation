e6d34697abfdf7b11e95ba29cec59d2574a39dcc
moki <morozov.kirill.moki@gmail.com>
2023-06-20 17:12:06 +0400

    test(markdown/rendere): imsize

--- src/__fixtures__/imsize.ts
+++ src/__fixtures__/imsize.ts
@@ -2,5 +2,16 @@ const markdown = `\
 параграф с ![картинкой внутри](image.png "альт текст" =13x37) с указанной шириной и высотой
 `;
 
-export {markdown};
-export default {markdown};
+const skeleton = `\
+%%%1%%% ![%%%2%%%](image.png "%%%3%%%" =13x37) %%%4%%%
+`;
+
+const translations = new Map<string, string>([
+    ['1', 'paragraph with'],
+    ['2', 'image inside'],
+    ['3', 'alt text'],
+    ['4', 'with width and height specified'],
+]);
+
+export {markdown, skeleton, translations};
+export default {markdown, skeleton, translations};
