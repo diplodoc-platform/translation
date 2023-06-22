9672a774c2d2f9c9caa7930a7a505e24fc5fe22e
moki <morozov.kirill.moki@gmail.com>
2023-06-22 16:17:03 +0400

    test(xlf/renderer): ref links {#T}

--- src/xlf/renderer.test.ts
+++ src/xlf/renderer.test.ts
@@ -16,6 +16,7 @@ import anchors from 'src/__fixtures__/anchors';
 import monospace from 'src/__fixtures__/monospace';
 import imsize from 'src/__fixtures__/imsize';
 import file from 'src/__fixtures__/file';
+import links from 'src/__fixtures__/links';
 
 describe('smoke', () => {
     test('it works', () => {
@@ -399,6 +400,25 @@ describe('xlf rendering', () => {
     it('handles markdown with file syntax', () => {
         const parameters = {
             markdown: file.markdown,
+            source: {
+                language: 'en',
+                locale: 'US' as const,
+            },
+            target: {
+                language: 'ru',
+                locale: 'RU' as const,
+            },
+            markdownPath: 'text.md',
+            skeletonPath: 'text.skl.md',
+        };
+
+        const rendered = render(parameters);
+        expect(rendered).toMatchSnapshot();
+    });
+
+    it('handles markdown with ref links({#T}) syntax', () => {
+        const parameters = {
+            markdown: links.markdown,
             source: {
                 language: 'en',
                 locale: 'US' as const,
