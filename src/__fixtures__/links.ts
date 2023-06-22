c20cb5ff1039e546e4c1a938018cce3a582e7bee
moki <morozov.kirill.moki@gmail.com>
2023-06-22 16:28:40 +0400

    test(markdown/renderer): ref links {#T}

--- src/__fixtures__/links.ts
+++ src/__fixtures__/links.ts
@@ -2,5 +2,15 @@ const markdown = `\
 параграф содержащий реф ссылку [{#T}](file.md "титул") продолжение предложения.
 `;
 
-export {markdown};
-export default {markdown};
+const skeleton = `\
+%%%1%%% [{#T}](file.md "%%%2%%%") %%%3%%%
+`;
+
+const translations = new Map<string, string>([
+    ['1', 'paragraph with ref link'],
+    ['2', 'title'],
+    ['3', 'paragraph continuation'],
+]);
+
+export {markdown, skeleton, translations};
+export default {markdown, skeleton, translations};
