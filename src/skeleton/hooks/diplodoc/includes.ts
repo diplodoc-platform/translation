92cfe4f5a24fa9413c5c252749ad6256f36bcb71
moki <morozov.kirill.moki@gmail.com>
2023-06-26 22:08:30 +0400

    chore(skeleton/renderer): factor out includes parser

--- src/skeleton/hooks/diplodoc/includes.ts
+++ src/skeleton/hooks/diplodoc/includes.ts
@@ -1,27 +1,16 @@
-import Token from 'markdown-it/lib/token';
 import {MarkdownRenderer} from '@diplodoc/markdown-it-markdown-renderer';
 import {CustomRendererHookParameters} from '@diplodoc/markdown-it-custom-renderer';
 
-const regexp = /^{%\s*include\s*(notitle)?\s*\[(.+?)]\((.+?)\)\s*%}$/;
+import {parseInclude} from 'src/include';
 
 function includes(this: MarkdownRenderer, parameters: CustomRendererHookParameters) {
     const {tokens} = parameters;
 
     for (let i = 0; i < tokens.length; i++) {
-        const {type, content} = tokens[i];
-        if (type !== 'inline') {
-            continue;
+        const {result, token} = parseInclude(tokens[i]);
+        if (result) {
+            tokens[i] = token;
         }
-
-        if (!regexp.test(content)) {
-            continue;
-        }
-
-        const include = new Token('include', '', 0);
-
-        include.content = content;
-
-        tokens[i] = include;
     }
 
     return '';
