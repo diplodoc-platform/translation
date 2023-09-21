269ab3ee5bb99fc797d4e47a40a6eeb4c80da160
moki <morozov.kirill.moki@gmail.com>
2023-09-21 22:34:28 +0400

    feat(skeleton)!: process inline render in bulk

--- src/skeleton/hooks/index.ts
+++ src/skeleton/hooks/index.ts
@@ -1,15 +1,19 @@
 import {MarkdownRendererLifeCycle} from '@diplodoc/markdown-it-markdown-renderer';
 
 import meta, {MetaParameters} from './meta';
+import {afterInline, AfterInlineState, initState} from './after-inline';
 import {includes} from './diplodoc';
 
+export type HooksState = AfterInlineState;
 export type HooksParameters = MetaParameters;
 
 function generate(parameters: HooksParameters) {
     return {
         hooks: {
             [MarkdownRendererLifeCycle.BeforeRender]: [meta.hook(parameters), includes],
+            [MarkdownRendererLifeCycle.AfterInlineRender]: [afterInline],
         },
+        initState: initState(),
     };
 }
 
---
+++ src/skeleton/renderer-inline.test.ts
@@ -0,0 +1,12 @@
+import {render, RenderParameters} from './renderer';
+
+describe('inline: skeleton rendering', () => {
+    it('inline: renders hash instead of the sentences with plain text.', () => {
+        const parameters: RenderParameters = {
+            markdown: 'Предложение номер один. Предложение номер два.',
+        };
+
+        const rendered = render(parameters);
+        expect(rendered).toMatchSnapshot();
+    });
+});
