9600447d498835fd33d9fb383cbb79ea313a65da
moki <morozov.kirill.moki@gmail.com>
2023-09-21 22:06:26 +0400

    feat(markdown)!: process inline render in bulk

--- src/markdown/handlers/diplodoc/file.ts
+++ src/markdown/handlers/diplodoc/file.ts
@@ -3,9 +3,9 @@ import Token from 'markdown-it/lib/token';
 
 import {replacer} from 'src/markdown/replacer';
 
-import {MarkdownHandlersState} from '../index';
+import {MarkdownRendererState} from 'src/markdown/renderer';
 
-function yfmFile(this: MarkdownRenderer<MarkdownHandlersState>, tokens: Token[], i: number) {
+function yfmFile(this: MarkdownRenderer<MarkdownRendererState>, tokens: Token[], i: number) {
     const token = tokens[i];
     const content = token.content;
 
