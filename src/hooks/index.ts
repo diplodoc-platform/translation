c63e4d595f85f375b5608a87aa5623461e23e5d0
ivan <ravestar@yandex-team.ru>
2023-10-04 15:07:43 +0300

    chore: cleanup code

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,7 +2,6 @@ import {CustomRendererHooks} from '@diplodoc/markdown-it-custom-renderer';
 
 export const mergeHooks = (...args: CustomRendererHooks[]): CustomRendererHooks => {
     const result: CustomRendererHooks = {};
-    // const keys = new Set([...Object.keys(defaultHooks), ...Object.keys(additionalHooks)]);
     const keys: Set<string> = new Set([...args.map(Object.keys)].flat());
     for (const key of keys) {
         result[key] = [...args.map((hook) => hook[key])].flat().filter(Boolean);
