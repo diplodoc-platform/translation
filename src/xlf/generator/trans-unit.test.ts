3969e421bedd35dc1ff2a411ba65c7ba51c56cb1
moki <morozov.kirill.moki@gmail.com>
2023-10-06 20:28:25 +0300

    rewrite(xlf/generator): trans-unit

--- src/xlf/generator/trans-unit.test.ts
+++ src/xlf/generator/trans-unit.test.ts
@@ -1,14 +1,14 @@
-import {generate} from './trans-unit';
+import {generateTransUnit} from './trans-unit';
 
 describe('trans-unit generator', () => {
     it('generates trans-unit with provided: source, id', () => {
-        const generated = generate({source: 'Sentence about something', id: 1});
+        const generated = generateTransUnit({source: 'Sentence about something', id: 1});
 
         expect(generated).toMatchSnapshot();
     });
 
     it('generates trans-unit with provided source, id, target', () => {
-        const generated = generate({
+        const generated = generateTransUnit({
             source: 'Sentence about something',
             target: 'Предложение о чем-то',
             id: 1,
