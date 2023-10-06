790310f786636c5ae8c0f743dd9eb87b1fd9b8d5
moki <morozov.kirill.moki@gmail.com>
2023-10-06 20:28:25 +0300

    rewrite(xlf/generator): trans-unit

--- src/xlf/generator/index.ts
+++ src/xlf/generator/index.ts
@@ -1,4 +1,4 @@
 export * as template from './template';
-export * as transUnit from './trans-unit';
+export {generateTransUnit, TransUnitParameters} from './trans-unit';
 
 export * as default from './index';
