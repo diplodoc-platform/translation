f60f01746a65f0c9a7cdd432335c38ca6b91d429
moki <morozov.kirill.moki@gmail.com>
2023-10-09 23:36:31 +0400

    feat(xlf/parser)!: parse XLF tokens

--- src/xlf/parser/parser.test.ts
+++ src/xlf/parser/parser.test.ts
@@ -1,5 +1,5 @@
 import {parseTranslations} from './index';
-import {generateTemplate, generateTransUnit} from 'src/xlf/generator';
+import {generateTemplate, generateTransUnit, TransUnitParameters} from 'src/xlf/generator';
 
 const templateParameters = {
     source: {language: 'en', locale: 'US' as const},
@@ -10,127 +10,89 @@ const templateParameters = {
 
 const {
     template: [before, after],
-    indentation,
 } = generateTemplate(templateParameters);
+
+const generateXLF = (units: TransUnitParameters[]) => before + units.map(generateTransUnit) + after;
 
 describe('smoke', () => {
     it('works', () => {
-        const transUnits = [
-            {
-                source: 'Sentence about something',
-                target: 'Предложение о чем-то',
-                id: 1,
-                indentation,
-            },
-            {source: 'Text fragment', target: 'Фрагмент Текста', id: 1, indentation},
+        const units = [
+            {target: 'Предложение о чем-то', id: 1},
+            {target: 'Фрагмент Текста', id: 2},
         ];
-        const xlf = before + transUnits.map(generateTransUnit).join('') + after;
-
+        const xlf = generateXLF(units);
         parseTranslations({xlf});
     });
 });
 
 describe('validates parameters', () => {
     it('works with valid parameters', () => {
-        const transUnits = [
-            {
-                source: 'Sentence about something',
-                target: 'Предложение о чем-то',
-                id: 1,
-                indentation,
-            },
-            {source: 'Text fragment', target: 'Фрагмент Текста', id: 1, indentation},
+        const units = [
+            {target: 'Предложение о чем-то', id: 1},
+            {target: 'Фрагмент Текста', id: 1},
         ];
-        const xlf = before + transUnits.map(generateTransUnit).join('') + after;
-
+        const xlf = generateXLF(units);
         parseTranslations({xlf});
     });
 
     it('throws on invalid parameters', () => {
         const invalidXLF = before + '</kek>' + after;
 
+        // empty xlf is invalid
         expect(() => parseTranslations({xlf: ''})).toThrow();
+        // invalid xml is invalid xlf
         expect(() => parseTranslations({xlf: invalidXLF})).toThrow();
     });
 });
 
 describe('parses translation units', () => {
     it('parses targets', () => {
-        const transUnits = [
-            {
-                source: 'Sentence about something',
-                target: 'Предложение о чем-то',
-                id: 1,
-                indentation,
-            },
-            {source: 'Text fragment', target: 'Фрагмент Текста', id: 2, indentation},
+        const units = [
+            {target: 'Предложение о чем-то', id: 1},
+            {target: 'Фрагмент Текста', id: 2},
         ];
-        const xlf = before + transUnits.map(generateTransUnit).join('') + after;
-
+        const xlf = generateXLF(units);
         const translations = parseTranslations({xlf});
 
-        for (const expected of transUnits) {
-            const translation = translations.get(String(expected.id));
-            if (!translation) {
-                throw new Error(`failed to receive ${expected} translation unit`);
-            }
+        expect(translations.length).toStrictEqual(2);
 
-            expect(translation).toStrictEqual(expected.target);
-        }
-    });
-
-    it('parses trans-units', () => {
-        const fixtures = [
-            {id: 1, target: 'text segment 1'},
-            {id: 2, target: 'text segment 2'},
-        ];
-        const xlf = before + fixtures.map(generateTransUnit).join('') + after;
-
-        const translations = parseTranslations({xlf});
-
-        for (const {id, target} of fixtures) {
-            const translation = translations.get(String(id));
-
-            if (!translation) {
-                throw new Error(`failed to receive ${id} translation unit`);
-            }
-
-            expect(translation).toStrictEqual(target);
+        for (let i = 0; i < translations.length; i++) {
+            const translation = translations[i];
+            const tokens = translation.filter(({type}) => type === 'text');
+            expect(tokens.length).toStrictEqual(1);
+            const data = tokens.reduce((str, {data}) => str + data, '');
+            expect(data).toStrictEqual(units[i].target);
         }
     });
 
     it('parses single trans-unit', () => {
-        const fixtures = [{id: 1, target: 'text segment 1'}];
-        const xlf = before + fixtures.map(generateTransUnit).join('') + after;
-
+        const units = [{target: 'Предложение о чем-то', id: 1}];
+        const xlf = generateXLF(units);
         const translations = parseTranslations({xlf});
 
-        for (const {id, target} of fixtures) {
-            const translation = translations.get(String(id));
-            if (!translation) {
-                throw new Error(`failed to receive ${id} translation unit`);
-            }
-
-            expect(translation).toStrictEqual(target);
-        }
+        expect(translations.length).toStrictEqual(1);
+        const tokens = translations[0].filter(({type}) => type === 'text');
+        expect(tokens.length).toStrictEqual(1);
+        const data = tokens.reduce((str, {data}) => str + data, '');
+        expect(data).toStrictEqual(units[0].target);
     });
 
-    it('parses trans-units sources when trans-unit targets are absent', () => {
-        const fixtures = [
-            {id: 1, source: 'текст 1'},
-            {id: 2, source: 'текст 2'},
+    it('parses sources when targets are absent with useSource flag enabled', () => {
+        const units = [
+            {source: 'Предложение о чем-то', id: 1},
+            {source: 'Фрагмент Текста', id: 2},
         ];
-        const xlf = before + fixtures.map(generateTransUnit).join('') + after;
-
+        const xlf = generateXLF(units);
         const translations = parseTranslations({xlf, useSource: true});
-        for (const {id, source} of fixtures) {
-            const translation = translations.get(String(id));
 
-            if (!translation) {
-                throw new Error(`failed to receive ${id} translation unit`);
-            }
+        expect(translations.length).toStrictEqual(2);
 
-            expect(translation).toStrictEqual(source);
+        for (let i = 0; i < translations.length; i++) {
+            const translation = translations[i];
+            const tokens = translation.filter(({type}) => type === 'text');
+            expect(tokens.length).toStrictEqual(1);
+            const data = tokens.reduce((str, {data}) => str + data, '');
+            expect(data).toStrictEqual(units[i].source);
         }
     });
 });
