b5ecc090caccf175b095d75bc487a27c43a50bc5
moki <morozov.kirill.moki@gmail.com>
2023-10-10 22:06:50 +0400

    refactor(api/compose): parser and renderer glue
    glue to parse and render xlf tokens into markdown as a separate module
    
    transform into convinient data structure for markdown renderer

--- src/xlf/index.ts
+++ src/xlf/index.ts
@@ -1,5 +1,6 @@
 export * as mdXLFRenderer from './renderer/md-xlf/';
 export * as generator from './generator';
 export * as parser from './parser';
+export * as translations from './translations';
 
 export * as default from './index';
