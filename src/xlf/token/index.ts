02bd279139fe273074aecce961d4a057006e3925
moki <morozov.kirill.moki@gmail.com>
2023-10-13 01:40:59 +0400

    feat(xlf/token/tag): add nodeType open/close
    distinguish between open and close nodes

--- src/xlf/token/index.ts
+++ src/xlf/token/index.ts
@@ -5,9 +5,12 @@ export type XLFTextToken = {
     data: string;
 };
 
+export type NodeTypes = 'open' | 'close';
+
 export type XLFTagToken = {
     type: 'tag';
     data: string;
+    nodeType: NodeTypes;
     ctype?: string;
     equivText?: string;
 };
@@ -16,8 +19,10 @@ function isXLFTextToken(token: XLFToken): token is XLFTextToken {
     return token?.type === 'text';
 }
 
+const xlfTagTokenNodeTypes = new Set(['open', 'close']);
+
 function isXLFTagToken(token: XLFToken): token is XLFTagToken {
-    return token?.type === 'tag';
+    return token?.type === 'tag' && xlfTagTokenNodeTypes.has(token.nodeType);
 }
 
 export {isXLFTextToken, isXLFTagToken};
