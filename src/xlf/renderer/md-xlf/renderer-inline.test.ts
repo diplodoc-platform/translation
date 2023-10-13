3ae1d2635da19c40c6bc50611d7679dcf5a8940b
moki <morozov.kirill.moki@gmail.com>
2023-10-13 14:18:15 +0400

    test(xlf/renderer/md-xlf): rename

--- src/xlf/renderer/md-xlf/renderer-inline.test.ts
+++ src/xlf/renderer/md-xlf/renderer-inline.test.ts
@@ -1,19 +1,23 @@
 import {render, RenderParameters} from './index';
 
-describe('inline: xlf rendering', () => {
-    it('inline: renders trans-unit for each sentence from paragraph with plain text.', () => {
+const baseRendererParameters = {
+    source: {
+        language: 'ru',
+        locale: 'RU' as const,
+    },
+    target: {
+        language: 'en',
+        locale: 'US' as const,
+    },
+    markdownPath: 'text.md',
+    skeletonPath: 'text.skl.md',
+};
+
+describe('renders xlf from markdown', () => {
+    it('renders plain text', () => {
         const parameters: RenderParameters = {
+            ...baseRendererParameters,
             markdown: 'Предложение номер один. Предложение номер два.',
-            source: {
-                language: 'ru',
-                locale: 'RU' as const,
-            },
-            target: {
-                language: 'en',
-                locale: 'US' as const,
-            },
-            markdownPath: 'text.md',
-            skeletonPath: 'text.skl.md',
         };
 
         const rendered = render(parameters);
