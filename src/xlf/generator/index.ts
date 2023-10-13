078c15560fb49de0a86d9cf53a869ddf286075d3
moki <morozov.kirill.moki@gmail.com>
2023-10-13 23:05:54 +0400

    feat(xlf/generators): <g> tag

--- src/xlf/generator/index.ts
+++ src/xlf/generator/index.ts
@@ -4,5 +4,6 @@ export {
     validParameters as templateValidParameters,
 } from './template';
 export {generateTransUnit, TransUnitParameters} from './trans-unit';
+export {generateOpenG, generateCloseG, OpenGParameters} from './g';
 
 export * as default from './index';
