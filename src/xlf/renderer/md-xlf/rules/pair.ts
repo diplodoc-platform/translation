150b8a4a41495651af424c5cfa0edb659637686e
moki <morozov.kirill.moki@gmail.com>
2023-10-13 23:07:27 +0400

    feat(xlf/renderer/md-xlf): strong
    handle simple paired syntax: strong

---
+++ src/xlf/renderer/md-xlf/rules/pair.ts
@@ -0,0 +1,26 @@
+import {CustomRenderer} from '@diplodoc/markdown-it-custom-renderer';
+import Renderer from 'markdown-it/lib/renderer';
+import Token from 'markdown-it/lib/token';
+
+import {XLFRendererState} from 'src/xlf/renderer/md-xlf/state';
+import {generateOpenG, generateCloseG} from 'src/xlf/generator';
+
+const pair: Renderer.RenderRuleRecord = {
+    strong_open: pairOpen,
+    strong_close: generateCloseG,
+};
+
+function pairOpen(this: CustomRenderer<XLFRendererState>, tokens: Token[], i: number) {
+    const {markup, tag, type} = tokens[i];
+    if (!markup?.length) {
+        throw new Error(`markup missing for token: ${type}`);
+    }
+    if (!tag?.length) {
+        throw new Error(`tag missing for token: ${type}`);
+    }
+
+    return generateOpenG({ctype: tag, equivText: markup});
+}
+
+export {pair};
+export default {pair};
