7b0167ce7bbe22301c8d103a24c5b51ec1c67a76
moki <morozov.kirill.moki@gmail.com>
2023-10-14 21:28:06 +0400

    test(markdown/renderer): em syntax

--- src/markdown/renderer-inline.test.ts
+++ src/markdown/renderer-inline.test.ts
@@ -26,4 +26,17 @@ describe('inline: renders translated markdown', () => {
         const rendered = render(parameters);
         expect(rendered).toMatchSnapshot();
     });
+
+    it('inline: renders sentences with em syntax', () => {
+        const parameters: RenderParameters = {
+            skeleton: '%%%1%%% %%%2%%%',
+            translations: new Map<string, string>([
+                ['1', 'Sentence number *one*.'],
+                ['2', 'Sentence number *two*.'],
+            ]),
+        };
+
+        const rendered = render(parameters);
+        expect(rendered).toMatchSnapshot();
+    });
 });
