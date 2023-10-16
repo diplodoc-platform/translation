d15a28e577fc227213c1de883ffc973576e2d4f8
moki <morozov.kirill.moki@gmail.com>
2023-10-17 02:13:58 +0400

    test(xlf/renderer/xlf-md): sup syntax

--- src/xlf/renderer/xlf-md/renderer-inline.test.ts
+++ src/xlf/renderer/xlf-md/renderer-inline.test.ts
@@ -96,4 +96,32 @@ describe('renders xlf to markdown', () => {
         const rendered = renderer.render(tokens);
         expect(rendered).toMatchSnapshot();
     });
+
+    it('renders sup wrapped in <g> tags', () => {
+        const renderer = new XLFMDRenderer();
+        const tokens: Array<XLFToken> = [
+            {type: 'tag', data: 'target', nodeType: 'open'},
+            {type: 'text', data: 'Предложение номер '},
+            {
+                type: 'tag',
+                data: 'g',
+                nodeType: 'open',
+                syntax: 'sup',
+                equivText: '^',
+            },
+            {type: 'text', data: 'один'},
+            {
+                type: 'tag',
+                data: 'g',
+                nodeType: 'close',
+                syntax: 'sup',
+                equivText: '^',
+            },
+            {type: 'text', data: '.'},
+            {type: 'tag', data: 'target', nodeType: 'close'},
+        ];
+
+        const rendered = renderer.render(tokens);
+        expect(rendered).toMatchSnapshot();
+    });
 });
