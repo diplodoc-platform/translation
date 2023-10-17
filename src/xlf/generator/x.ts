b37974117016131e2f19abcea4da61049b86b88c
moki <morozov.kirill.moki@gmail.com>
2023-10-17 15:00:10 +0400

    feat(xlf/generator): x tag

---
+++ src/xlf/generator/x.ts
@@ -0,0 +1,31 @@
+import {lt, sl, qt, gt} from 'src/xlf/symbols';
+
+export type GenerateXParameters = {
+    ctype: string;
+    equivText: string;
+};
+
+function generateX(parameters: GenerateXParameters) {
+    const {ctype, equivText} = parameters;
+
+    let rendered = `${lt}x`;
+
+    if (ctype?.length) {
+        rendered += ` ctype=${qt}x-${ctype}${qt}`;
+    }
+
+    if (equivText?.length) {
+        rendered += ` equiv-text=${qt}${equivText}${qt}`;
+    }
+
+    if (ctype?.length || equivText?.length) {
+        rendered += ' ';
+    }
+
+    rendered += sl + gt;
+
+    return rendered;
+}
+
+export {generateX};
+export default {generateX};
