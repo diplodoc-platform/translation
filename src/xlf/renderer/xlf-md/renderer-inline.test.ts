df3bd05b9235d54ced87cf89b8ada953689e6945
moki <morozov.kirill.moki@gmail.com>
2023-10-17 12:41:50 +0400

    test(xlf/renderer/xlf-md): samp syntax

--- src/xlf/renderer/xlf-md/renderer-inline.test.ts
+++ src/xlf/renderer/xlf-md/renderer-inline.test.ts
@@ -124,4 +124,32 @@ describe('renders xlf to markdown', () => {
         const rendered = renderer.render(tokens);
         expect(rendered).toMatchSnapshot();
     });
+
+    it('renders samp wrapped in <g> tags', () => {
+        const renderer = new XLFMDRenderer();
+        const tokens: Array<XLFToken> = [
+            {type: 'tag', data: 'target', nodeType: 'open'},
+            {type: 'text', data: 'Предложение номер '},
+            {
+                type: 'tag',
+                data: 'g',
+                nodeType: 'open',
+                syntax: 'samp',
+                equivText: '##',
+            },
+            {type: 'text', data: 'один'},
+            {
+                type: 'tag',
+                data: 'g',
+                nodeType: 'close',
+                syntax: 'samp',
+                equivText: '##',
+            },
+            {type: 'text', data: '.'},
+            {type: 'tag', data: 'target', nodeType: 'close'},
+        ];
+
+        const rendered = renderer.render(tokens);
+        expect(rendered).toMatchSnapshot();
+    });
 });
