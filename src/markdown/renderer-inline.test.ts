9328d7bbb7aedc319bbcda097427dd3893714338
moki <morozov.kirill.moki@gmail.com>
2023-10-17 11:58:06 +0400

    test(markdown/renderer): samp syntax

--- src/markdown/renderer-inline.test.ts
+++ src/markdown/renderer-inline.test.ts
@@ -65,4 +65,17 @@ describe('inline: renders translated markdown', () => {
         const rendered = render(parameters);
         expect(rendered).toMatchSnapshot();
     });
+
+    it('inline: renders sentences with samp syntax', () => {
+        const parameters: RenderParameters = {
+            skeleton: '%%%1%%% %%%2%%%',
+            translations: new Map<string, string>([
+                ['1', 'Sentence number ##one##.'],
+                ['2', 'Sentence number ##two##.'],
+            ]),
+        };
+
+        const rendered = render(parameters);
+        expect(rendered).toMatchSnapshot();
+    });
 });
