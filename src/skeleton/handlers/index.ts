277b7bcbfb414e73dd778955656f429eae45150a
moki <morozov.kirill.moki@gmail.com>
2023-10-18 13:38:42 +0400

    feat(skeleton/renderer): links syntax
    disable segmenter inside links handler
    
    (we are now segmenting at the AfterInlineRender stage)

--- src/skeleton/handlers/index.ts
+++ src/skeleton/handlers/index.ts
@@ -1,4 +1,3 @@
-import {linkClose} from './link';
 import {imageClose} from './image';
 import {yfmFile} from './diplodoc/file';
 
@@ -8,7 +7,6 @@ function generate() {
 
 function handlers() {
     return {
-        link_close: linkClose,
         image_close: imageClose,
         yfm_file: yfmFile,
     };
