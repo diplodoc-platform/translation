1ecfc93edfdb8b0b302dc3ea80c94e55ac7d7019
moki <morozov.kirill.moki@gmail.com>
2023-10-18 19:06:11 +0400

    feat(xlf/renderer/md-xlf): link syntax

--- src/xlf/renderer/md-xlf/rules/link.ts
+++ src/xlf/renderer/md-xlf/rules/link.ts
@@ -1,9 +1,9 @@
 import {CustomRenderer} from '@diplodoc/markdown-it-custom-renderer';
+import Renderer from 'markdown-it/lib/renderer';
 import Token from 'markdown-it/lib/token';
 
-import {segmenter} from 'src/xlf/renderer/md-xlf/segmenter';
-
 import {XLFRendererState} from 'src/xlf/renderer/md-xlf/state';
+import {generateOpenG, generateCloseG, generateX} from 'src/xlf/generator';
 
 export type LinkRuleState = {
     link: {
@@ -19,10 +19,17 @@ function initState() {
     };
 }
 
+const link: Renderer.RenderRuleRecord = {
+    link_open: linkOpen,
+    link_close: linkClose,
+};
+
 function linkOpen(this: CustomRenderer<XLFRendererState>, tokens: Token[], i: number) {
     this.state.link.pending.push(tokens[i]);
 
-    return '';
+    const rendered = generateOpenG({ctype: 'link_text_part', equivText: '[]'});
+
+    return rendered;
 }
 
 function linkClose(this: CustomRenderer<XLFRendererState>) {
@@ -31,18 +38,32 @@ function linkClose(this: CustomRenderer<XLFRendererState>) {
         throw new Error('failed to render trans-unit from link');
     }
 
+    let rendered = generateCloseG();
+
+    rendered += generateOpenG({ctype: 'link_attributes_part', equivText: '()'});
+
+    const href = token.attrGet('href');
+    if (href?.length) {
+        rendered += generateX({ctype: 'link_attributes_href', equivText: `href="${href}"`});
+    }
+
     const title = token.attrGet('title');
     if (!title?.length) {
         this.state.link.pending.push(token);
-        return '';
+
+        rendered += generateCloseG();
+
+        return rendered;
     }
 
-    let rendered = '';
+    rendered += generateOpenG({ctype: 'link_attributes_title', equivText: '""'});
+    rendered += title;
+    rendered += generateCloseG();
 
-    rendered += segmenter(title, this.state);
+    rendered += generateCloseG();
 
     return rendered;
 }
 
-export {linkOpen, linkClose, initState};
+export {link, initState};
 export default {linkOpen, linkClose, initState};
