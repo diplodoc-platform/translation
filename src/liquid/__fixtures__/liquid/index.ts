22ba1047dd9977fd10d5697344cf37a55a3928f4
moki <morozov.kirill.moki@gmail.com>
2023-11-13 15:00:19 +0400

    test(liquid/tokenizer): liquid and variables syntax

---
+++ src/liquid/__fixtures__/liquid/index.ts
@@ -0,0 +1,8 @@
+export * from './fixture';
+export * from './text';
+export * from './condition';
+export * from './functions';
+export * from './filter';
+export * from './variable';
+export * from './loop';
+export * as default from './index';
