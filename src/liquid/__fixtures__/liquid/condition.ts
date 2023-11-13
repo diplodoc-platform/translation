35a3c85c59275511ab7f62e2f6eb11ee950a1073
moki <morozov.kirill.moki@gmail.com>
2023-11-13 15:00:19 +0400

    test(liquid/tokenizer): liquid and variables syntax

---
+++ src/liquid/__fixtures__/liquid/condition.ts
@@ -0,0 +1,33 @@
+import {Fixtures} from './fixture';
+
+const condition: Fixtures = [
+    {
+        section: 'conditions',
+        content: "Person is: {% if personType == 'cool' %} cool {% else %} not cool.",
+        number: 1,
+    },
+    {
+        section: 'conditions',
+        content:
+            '{%if var == "a"%} A {%else%} B was the point we examined. Now examine let\'s examine others.',
+        number: 2,
+    },
+    {
+        section: 'conditions',
+        content: `{% if var == "a" %}
+
+Content when var is equal a
+
+{% else %}
+
+Content when var is not equal a
+
+{% endif %}
+
+`,
+        number: 3,
+    },
+];
+
+export {condition};
+export default {condition};
