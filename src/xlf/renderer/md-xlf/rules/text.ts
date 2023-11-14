0e154df7a170d40df5ff36c279f0cbbc292c6927
moki <morozov.kirill.moki@gmail.com>
2023-11-14 18:22:05 +0400

    feat(xlf/renderer/md-xlf): liquid conditions

--- src/xlf/renderer/md-xlf/rules/text.ts
+++ src/xlf/renderer/md-xlf/rules/text.ts
@@ -1,16 +1,10 @@
 import {CustomRenderer} from '@diplodoc/markdown-it-custom-renderer';
 import Renderer from 'markdown-it/lib/renderer';
 import Token from 'markdown-it/lib/token';
+import {Tokenizer} from 'src/liquid/tokenizer';
+import {Renderer as LiquidRenderer} from 'src/liquid';
 
 import {XLFRendererState} from 'src/xlf/renderer/md-xlf/state';
-
-// disabled for now
-// handle ref links {#T}
-// src/__fixtures__/links.ts
-//
-// handle anchors # title {#anchor}
-// src/__fixtures__/anchors.ts
-//
 
 const text: Renderer.RenderRuleRecord = {
     text: textRule,
@@ -25,7 +19,11 @@ function textRule(this: CustomRenderer<XLFRendererState>, tokens: Token[], i: nu
         return '';
     }
 
-    return content;
+    const tokenizer = new Tokenizer(content);
+    const liquidTokens = tokenizer.tokenize();
+    const renderer = new LiquidRenderer(liquidTokens);
+
+    return renderer.render();
 }
 
 export {text};
