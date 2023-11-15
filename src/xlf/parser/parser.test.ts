01cdc8884c404702c8b8d88b770599a23c869725
moki <morozov.kirill.moki@gmail.com>
2023-11-15 19:19:41 +0400

    test(xlf/parser): liquid variables

--- src/xlf/parser/parser.test.ts
+++ src/xlf/parser/parser.test.ts
@@ -400,4 +400,17 @@ describe('parses translation units with <g> and <x> tags', () => {
         expect(translations.length).toStrictEqual(units.length);
         expect(translations).toMatchSnapshot();
     });
+
+    it('parses liquid variables wrapped in <x> tags', () => {
+        const units = [
+            {
+                id: 1,
+                target: 'Sentence with <x ctype="x-liquid_Variable" equiv-text="{{ variable }}" />.',
+            },
+        ];
+        const xlf = generateXLF(units);
+        const translations = parseTranslations({xlf});
+        expect(translations.length).toStrictEqual(units.length);
+        expect(translations).toMatchSnapshot();
+    });
 });
