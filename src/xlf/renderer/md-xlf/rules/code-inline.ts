7fdd2ac62bc5879e82c7682989bcc9479d621ef5
moki <morozov.kirill.moki@gmail.com>
2023-11-21 20:31:09 +0400

    fix(xlf/renderer/md-xlf): wrap inline code in <g> tag

--- src/xlf/renderer/md-xlf/rules/code-inline.ts
+++ src/xlf/renderer/md-xlf/rules/code-inline.ts
@@ -3,16 +3,25 @@ import Renderer from 'markdown-it/lib/renderer';
 import Token from 'markdown-it/lib/token';
 
 import {XLFRendererState} from 'src/xlf/renderer/md-xlf/state';
-import {generateX} from 'src/xlf/generator';
+import {generateOpenG, generateCloseG} from 'src/xlf/generator';
 
 const codeInline: Renderer.RenderRuleRecord = {
     code_inline: codeInlineRule,
 };
 
 function codeInlineRule(this: CustomRenderer<XLFRendererState>, tokens: Token[], i: number) {
-    const {markup, content, tag} = tokens[i];
+    const {markup, content, tag, type} = tokens[i];
+    if (!markup?.length) {
+        throw new Error(`markup missing for token: ${type}`);
+    }
 
-    return generateX({ctype: tag, equivText: `${markup}${content}${markup}`});
+    let rendered = '';
+
+    rendered += generateOpenG({ctype: tag, equivText: markup});
+    rendered += content;
+    rendered += generateCloseG();
+
+    return rendered;
 }
 
 export {codeInline};
