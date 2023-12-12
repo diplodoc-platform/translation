52fd0ee40e762b77b2b01f3f0488001d068ab3be
moki <morozov.kirill.moki@gmail.com>
2023-12-12 21:35:45 +0400

    fix(xlf/renderer/md-xlf): inline code with liquid

--- src/xlf/renderer/md-xlf/rules/code-inline.ts
+++ src/xlf/renderer/md-xlf/rules/code-inline.ts
@@ -4,6 +4,8 @@ import Token from 'markdown-it/lib/token';
 
 import {XLFRendererState} from 'src/xlf/renderer/md-xlf/state';
 import {generateCloseG, generateOpenG} from 'src/xlf/generator';
+import {Tokenizer} from 'src/liquid/tokenizer';
+import {Renderer as LiquidRenderer} from 'src/liquid';
 
 const codeInline: Renderer.RenderRuleRecord = {
     code_inline: codeInlineRule,
@@ -15,10 +17,14 @@ function codeInlineRule(this: CustomRenderer<XLFRendererState>, tokens: Token[],
         throw new Error(`markup missing for token: ${type}`);
     }
 
+    const tokenizer = new Tokenizer(content);
+    const liquidTokens = tokenizer.tokenize();
+    const renderer = new LiquidRenderer(liquidTokens);
+
     let rendered = '';
 
     rendered += generateOpenG({ctype: tag, equivText: markup});
-    rendered += content;
+    rendered += renderer.render();
     rendered += generateCloseG();
 
     return rendered;
