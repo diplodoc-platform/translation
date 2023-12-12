fe0514965c6860214b854ccd5d5f91c0bb0f94dd
moki <morozov.kirill.moki@gmail.com>
2023-12-12 19:49:38 +0400

    test(markdown/renderer): abbreviations inside parenthesis

--- src/markdown/renderer-inline.test.ts
+++ src/markdown/renderer-inline.test.ts
@@ -312,4 +312,17 @@ describe('inline: renders translated markdown', () => {
         const rendered = render(parameters);
         expect(rendered).toMatchSnapshot();
     });
+
+    it('inline: renders sentences with abbreviations inside parenthesis', () => {
+        const parameters: RenderParameters = {
+            skeleton: `%%%1%%% %%%2%%%`,
+            translations: new Map<string, string>([
+                ['1', 'Sentence (см. [link](file.md)) continues.'],
+                ['2', 'Another Sentence.'],
+            ]),
+        };
+
+        const rendered = render(parameters);
+        expect(rendered).toMatchSnapshot();
+    });
 });
