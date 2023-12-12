cbf0ffd8a05244a1c9d9db5b4b4e20752b0f71ab
moki <morozov.kirill.moki@gmail.com>
2023-12-12 20:41:03 +0400

    test(markdown/renderer): inline code with liquid

--- src/markdown/renderer-inline.test.ts
+++ src/markdown/renderer-inline.test.ts
@@ -325,4 +325,17 @@ describe('inline: renders translated markdown', () => {
         const rendered = render(parameters);
         expect(rendered).toMatchSnapshot();
     });
+
+    it('inline: renders sentences with inline code that has liquid syntax inside', () => {
+        const parameters: RenderParameters = {
+            skeleton: `%%%1%%% %%%2%%%`,
+            translations: new Map<string, string>([
+                ['1', 'Sentence with `inline code and {{ui-key.yacloud.common.label_tcp}}`.'],
+                ['2', 'Another Sentence.'],
+            ]),
+        };
+
+        const rendered = render(parameters);
+        expect(rendered).toMatchSnapshot();
+    });
 });
