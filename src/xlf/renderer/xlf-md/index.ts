056000894668404d2cc3e44463eda3e07825a70b
moki <morozov.kirill.moki@gmail.com>
2024-01-17 17:00:02 +0400

    fix(xlf/renderer/xlf-md): x inline markup

--- src/xlf/renderer/xlf-md/index.ts
+++ src/xlf/renderer/xlf-md/index.ts
@@ -15,43 +15,78 @@ class XLFMDRenderer {
         this.rules = {
             text: this.text.bind(this),
             tag: this.tag.bind(this),
-            g: this.g.bind(this),
-            strong: this.strong.bind(this),
-            em: this.em.bind(this),
-            s: this.s.bind(this),
-            sup: this.sup.bind(this),
-            samp: this.samp.bind(this),
             x: this.x.bind(this),
-            code: this.code.bind(this),
-            link_text_part: this.linkTextPart.bind(this),
-            link_attributes_part: this.linkAttributesPart.bind(this),
-            link_attributes_title: this.linkAttributesTitle.bind(this),
-            link_attributes_href: this.linkAttributesHref.bind(this),
-            link_reflink: this.linkRefLink.bind(this),
-            link_autolink: this.linkAutolink.bind(this),
-            image_text_part: this.imageTextPart.bind(this),
-            image_attributes_part: this.imageAttributesPart.bind(this),
-            image_attributes_src: this.imageAttributesSrc.bind(this),
-            image_attributes_title: this.imageAttributesTitle.bind(this),
-            image_attributes_size: this.imageAttributesSize.bind(this),
-            video: this.video.bind(this),
-            anchor: this.anchor.bind(this),
-            file: this.file.bind(this),
-            file_src: this.fileAttributeNonTranslatable.bind(this),
-            file_name: this.fileAttributeTranslatable.bind(this),
-            file_referrerpolicy: this.fileAttributeNonTranslatable.bind(this),
-            file_rel: this.fileAttributeNonTranslatable.bind(this),
-            file_target: this.fileAttributeNonTranslatable.bind(this),
-            file_type: this.fileAttributeNonTranslatable.bind(this),
-            liquid_If: this.liquidAsIs.bind(this),
-            liquid_Else: this.liquidAsIs.bind(this),
-            liquid_EndIf: this.liquidAsIs.bind(this),
-            liquid_ForInLoop: this.liquidAsIs.bind(this),
-            liquid_EndForInLoop: this.liquidAsIs.bind(this),
-            liquid_Function: this.liquidAsIs.bind(this),
-            liquid_Filter: this.liquidAsIs.bind(this),
-            liquid_Variable: this.liquidAsIs.bind(this),
-            html_inline: this.htmlInline.bind(this),
+
+            // simple pair syntax
+            // strong
+            strong_open: this.literal.bind(this),
+            strong_close: this.literal.bind(this),
+            // em
+            em_open: this.literal.bind(this),
+            em_close: this.literal.bind(this),
+            // s
+            s_open: this.literal.bind(this),
+            s_close: this.literal.bind(this),
+            // sup
+            sup_open: this.literal.bind(this),
+            sup_close: this.literal.bind(this),
+            // samp
+            samp_open: this.literal.bind(this),
+            samp_close: this.literal.bind(this),
+            // code
+            code_open: this.literal.bind(this),
+            code_close: this.literal.bind(this),
+
+            // link
+            link_text_part_open: this.literal.bind(this),
+            link_text_part_close: this.literal.bind(this),
+            link_attributes_part_open: this.literal.bind(this),
+            link_attributes_part_close: this.literal.bind(this),
+            link_attributes_href: this.literal.bind(this),
+            link_attributes_title_open: this.prependSpaceLiteral.bind(this),
+            link_attributes_title_close: this.literal.bind(this),
+            link_autolink: this.literal.bind(this),
+            link_reflink: this.literal.bind(this),
+
+            // image
+            image_text_part_open: this.literal.bind(this),
+            image_text_part_close: this.literal.bind(this),
+            image_attributes_part_open: this.literal.bind(this),
+            image_attributes_part_close: this.literal.bind(this),
+            image_attributes_src: this.literal.bind(this),
+            image_attributes_title_open: this.prependSpaceLiteral.bind(this),
+            image_attributes_title_close: this.literal.bind(this),
+            image_attributes_size: this.prependSpaceLiteral.bind(this),
+
+            // video
+            video: this.literal.bind(this),
+
+            // anchor
+            anchor: this.literal.bind(this),
+
+            // file
+            file_open: this.literal.bind(this),
+            file_src: this.prependSpaceLiteral.bind(this),
+            file_name_open: this.prependSpaceLiteral.bind(this),
+            file_name_close: this.literal.bind(this),
+            file_referrerpolicy: this.prependSpaceLiteral.bind(this),
+            file_rel: this.prependSpaceLiteral.bind(this),
+            file_target: this.prependSpaceLiteral.bind(this),
+            file_type: this.prependSpaceLiteral.bind(this),
+            file_close: this.prependSpaceLiteral.bind(this),
+
+            // liquid
+            liquid_If: this.literal.bind(this),
+            liquid_Else: this.literal.bind(this),
+            liquid_EndIf: this.literal.bind(this),
+            liquid_ForInLoop: this.literal.bind(this),
+            liquid_EndForInLoop: this.literal.bind(this),
+            liquid_Function: this.literal.bind(this),
+            liquid_Filter: this.literal.bind(this),
+            liquid_Variable: this.literal.bind(this),
+
+            // html
+            html_inline: this.literal.bind(this),
         };
     }
 
@@ -101,299 +136,19 @@ class XLFMDRenderer {
         return handler(token);
     }
 
-    g(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const syntax = token.syntax;
-        if (!syntax?.length) {
-            throw new Error("can't render g tag without syntax");
-        }
-
-        const handler = this.rules[syntax];
-        if (!handler) {
-            throw new Error(`syntax ${syntax} not implemented`);
-        }
-
-        return handler(token);
+    prependSpaceLiteral(token: XLFToken): string {
+        return ' ' + this.literal(token);
     }
 
-    strong(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-        if (!token?.equivText) {
-            throw new Error(`token: ${token} missing equiv-text`);
-        }
-
-        return token.equivText ?? '**';
-    }
-
-    em(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-        if (!token?.equivText) {
-            throw new Error(`token: ${token} missing equiv-text`);
-        }
-
-        return token.equivText ?? '*';
-    }
-
-    s(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        if (!token?.equivText) {
-            throw new Error(`token: ${token} missing equiv-text`);
-        }
-
-        return token.equivText ?? '~~';
-    }
-
-    sup(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        if (!token?.equivText) {
-            throw new Error(`token: ${token} missing equiv-text`);
-        }
-
-        return token.equivText ?? '^';
-    }
-
-    samp(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        if (!token?.equivText) {
-            throw new Error(`token: ${token} missing equiv-text`);
-        }
-
-        return token.equivText ?? '##';
-    }
-
-    linkTextPart(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const {equivText, nodeType} = token;
-
-        if (equivText?.length !== 2) {
-            throw new Error(`token: ${token} has invalid equiv-text`);
-        }
-
-        const [open, close] = equivText.split('');
-
-        return nodeType === 'open' ? open : close;
-    }
-
-    linkAttributesPart(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const {equivText, nodeType} = token;
-
-        if (equivText?.length !== 2) {
-            throw new Error(`token: ${token} has invalid equiv-text`);
-        }
-
-        const [open, close] = equivText.split('');
-
-        return nodeType === 'open' ? open : close;
-    }
-
-    linkAttributesHref(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        assert(
-            token.equivText?.length,
-            `x supposed to wrap original link href markup inside equiv-text`,
-        );
-
-        return token.equivText;
-    }
-
-    linkAttributesTitle(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const {equivText, nodeType} = token;
-
-        if (equivText?.length !== 2) {
-            throw new Error(`token: ${token} has invalid equiv-text`);
-        }
-
-        const [open, close] = equivText.split('');
-
-        return nodeType === 'open' ? ' ' + open : close;
-    }
-
-    linkRefLink(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        assert(
-            token.equivText?.length,
-            `x supposed to wrap original ref link markup inside equiv-text`,
-        );
-
-        return token.equivText;
-    }
-
-    linkAutolink(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        assert(
-            token.equivText?.length,
-            `x supposed to wrap original ref link markup inside equiv-text`,
-        );
-
-        return token.equivText;
-    }
-
-    imageTextPart(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const {equivText, nodeType} = token;
-        if (equivText?.length !== 3) {
-            throw new Error(`token: ${token} has invalid equiv-text`);
-        }
-
-        const [open, close] = equivText.split(/(\]$)/gmu);
-        return nodeType === 'open' ? open : close;
-    }
-
-    imageAttributesPart(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const {equivText, nodeType} = token;
-        if (equivText?.length !== 2) {
-            throw new Error(`token: ${token} has invalid equiv-text`);
-        }
-
-        const [open, close] = equivText.split('');
-
-        return nodeType === 'open' ? open : close;
-    }
-
-    imageAttributesSrc(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        if (!token?.equivText?.length) {
-            throw new Error(`x supposed to wrap image src inside equiv-text`);
-        }
-
-        return token.equivText;
-    }
-
-    imageAttributesTitle(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const {equivText, nodeType} = token;
-        if (equivText?.length !== 2) {
-            throw new Error(`token: ${token} has invalid equiv-text`);
-        }
-
-        const [open, close] = equivText.split('');
-
-        return nodeType === 'open' ? ' ' + open : close;
-    }
-
-    imageAttributesSize(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        assert(token.equivText?.length, `token: ${token} has invalid equiv-text`);
-
-        return ' ' + token.equivText;
-    }
-
-    code(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        assert(token.equivText?.length, `token: ${token} has invalid equiv-text`);
-
-        return token.equivText;
-    }
-
-    video(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        assert(token.equivText?.length, `token: ${token} has invalid equiv-text`);
-
-        return token.equivText;
-    }
-
-    anchor(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        assert(token.equivText?.length, `token: ${token} has invalid equiv-text`);
-
-        return token.equivText;
-    }
-
-    file(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const {equivText, nodeType} = token;
-        assert(equivText?.length === 4, `token: ${token} has invalid equiv-text`);
-
-        const [open, close] = equivText.split(/(%\}$)/gmu);
-
-        return nodeType === 'open' ? `${open} file` : ` ${close}`;
-    }
-
-    fileAttributeNonTranslatable(token: XLFToken): string {
+    literal(token: XLFToken): string {
         assert(isXLFTagToken(token));
         token as XLFTagToken;
 
         const {equivText} = token;
-        assert(equivText?.length, `token: ${token} has invalid equiv-text`);
-
-        return equivText;
-    }
-
-    fileAttributeTranslatable(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const {syntax, nodeType, equivText} = token;
-        assert(syntax?.length, `token: ${token} has invalid syntax name`);
-
-        const [_, name] = syntax.split('_');
-        assert(name?.length, `token: ${token} has invalid syntax name`);
-        assert(equivText?.length === 2, `token: ${token} has invalid equiv-text`);
-
-        const [open, close] = equivText.split('');
-
-        return nodeType === 'open' ? ` ${name}=${open}` : close;
-    }
-
-    liquidAsIs(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const {equivText} = token;
-        assert(equivText?.length, `token: ${token} has invalid equiv-text`);
-
-        return equivText;
-    }
-
-    htmlInline(token: XLFToken): string {
-        assert(isXLFTagToken(token));
-        token as XLFTagToken;
-
-        const {equivText} = token;
-        assert(equivText?.length, `token: ${token} has invalid equiv-text`);
+        assert(
+            equivText?.length,
+            'literal tag should contain original markup inside equiv-text attritbute',
+        );
 
         return equivText;
     }
