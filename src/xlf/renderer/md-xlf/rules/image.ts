b38ca6dc0fe8f283ca4bc733a5198f8dc46b758e
moki <morozov.kirill.moki@gmail.com>
2024-01-17 16:49:41 +0400

    fix(xlf/renderer/md-xlf): wrap image in x tags

--- src/xlf/renderer/md-xlf/rules/image.ts
+++ src/xlf/renderer/md-xlf/rules/image.ts
@@ -1,8 +1,11 @@
+import MarkdownIt from 'markdown-it';
 import {CustomRenderer} from '@diplodoc/markdown-it-custom-renderer';
 import Token from 'markdown-it/lib/token';
 import {XLFRendererState} from 'src/xlf/renderer/md-xlf/state';
 
-import {generateCloseG, generateOpenG, generateX} from 'src/xlf/generator';
+import {generateX} from 'src/xlf/generator';
+
+const decodeURL = new MarkdownIt().utils.lib.mdurl.decode;
 
 const image = {
     image_open: imageOpen,
@@ -10,34 +13,52 @@ const image = {
 };
 
 function imageOpen(this: CustomRenderer<XLFRendererState>) {
-    return generateOpenG({ctype: 'image_text_part', equivText: '![]'});
+    return generateX({
+        ctype: 'image_text_part_open',
+        equivText: '![',
+    });
 }
 
 function imageClose(this: CustomRenderer<XLFRendererState>, tokens: Token[], i: number) {
     const token = tokens[i];
     let rendered = '';
 
-    rendered += generateCloseG();
+    rendered += generateX({
+        ctype: 'image_text_part_close',
+        equivText: ']',
+    });
 
-    rendered += generateOpenG({ctype: 'image_attributes_part', equivText: '()'});
+    rendered += generateX({
+        ctype: 'image_attributes_part_open',
+        equivText: '(',
+    });
 
-    const src = token.attrGet('src');
+    let src = token.attrGet('src');
     if (src?.length) {
-        rendered += generateX({ctype: 'image_attributes_src', equivText: src});
+        src = decodeURL(src);
+        rendered += generateX({
+            ctype: 'image_attributes_src',
+            equivText: src,
+        });
     }
 
     const title = token.attrGet('title');
     if (title?.length) {
-        rendered += generateOpenG({ctype: 'image_attributes_title', equivText: '""'});
+        rendered += generateX({
+            ctype: 'image_attributes_title_open',
+            equivText: '"',
+        });
 
         rendered += title;
 
-        rendered += generateCloseG();
+        rendered += generateX({
+            ctype: 'image_attributes_title_close',
+            equivText: '"',
+        });
     }
 
     const height = token.attrGet('height');
     const width = token.attrGet('width');
-
     if (width?.length || height?.length) {
         let equivText = '=';
 
@@ -54,7 +75,10 @@ function imageClose(this: CustomRenderer<XLFRendererState>, tokens: Token[], i: 
         rendered += generateX({ctype: 'image_attributes_size', equivText});
     }
 
-    rendered += generateCloseG();
+    rendered += generateX({
+        ctype: 'image_attributes_part_close',
+        equivText: ')',
+    });
 
     return rendered;
 }
