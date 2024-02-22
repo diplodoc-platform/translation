a618237b622cc93d23a8accdfa638341e7da9a05
3y3 <3y3@ya.ru>
2024-02-22 15:00:07 +0300

    fix: Fix types

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
-export {extract, ExtractOptions, compose, ComposeOptions} from './api';
+export type {JSONValue} from './json';
+export type {ExtractOptions, ComposeOptions} from './api';
 
+export {extract, compose} from './api';
 export {resolve as resolveRefs} from './json';
