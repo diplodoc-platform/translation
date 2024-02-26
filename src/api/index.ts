2ab5a2d203e42d3e9c9fbdb277b5a33ae7b70405
3y3 <3y3@ya.ru>
2024-02-26 13:01:11 +0300

    fix: types

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,29 +1,35 @@
-import type {ComposeOptions as MdComposeOptions, ExtractOptions as MdExtractOptions, ExtractOutput as MdExtractOutput } from './md';
-import type {ComposeOptions as JsonComposeOptions, ExtractOptions as JsonExtractOptions, ExtractOutput as JsonExtractOutput } from './json';
+import type {ComposeOptions as MdComposeOptions, ExtractOptions as MdExtractOptions} from './md';
+import type {ComposeOptions as JsonComposeOptions, ExtractOptions as JsonExtractOptions } from './json';
 import {compose as composeMd, extract as extraactMd } from './md';
 import {compose as composeJson, extract as extraactJson} from './json';
 import {validate} from './validate';
 import {JSONValue} from 'src/json';
 
-export type ExtractOptions = Parameters<typeof extract>[1];
+export type ExtractOptions = JsonExtractOptions | MdExtractOptions;
 
-export type ComposeOptions = Parameters<typeof compose>[2];
+export type ExtractOutput<T extends string | JSONValue> = {
+    skeleton: T;
+    xliff: string;
+    units: string[];
+};
 
-export function extract(content: JSONValue, params: JsonComposeOptions): JsonExtractOutput
-export function extract(content: string, params: MdExtractOptions): MdExtractOutput
-export function extract(content: any, params: any): any {
-    validate('ExtractOptions', params);
+export function extract(content: string, params: MdExtractOptions): ExtractOutput<string>
+export function extract(content: JSONValue, params: JsonExtractOptions): ExtractOutput<JSONValue>
+export function extract(content: any, options: any): ExtractOutput<any> {
+    validate('ExtractOptions', options);
 
     const type = typeof content === 'string' ? 'md' : 'json';
 
-    return extract[type](content, params);
+    return extract[type](content, options);
 }
 
 extract.md = extraactMd;
 extract.json = extraactJson;
 
-export function compose(skeleton: JSONValue, xliff: string | string[], options: JsonExtractOptions): JSONValue
+export type ComposeOptions = JsonComposeOptions | MdComposeOptions;
+
 export function compose(skeleton: string, xliff: string | string[], options: MdComposeOptions): string
+export function compose(skeleton: JSONValue, xliff: string | string[], options: JsonComposeOptions): JSONValue
 export function compose(skeleton: any, xliff: string | string[], options: any): any {
     validate('ComposeOptions', options);
 
