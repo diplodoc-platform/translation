25420b7d34f5f6a3815886c1c89c68f2d6ba24c1
3y3 <3y3@ya.ru>
2024-07-12 14:23:04 +0300

    fix: Handle liquid elsif

--- src/skeleton/liquid/token.ts
+++ src/skeleton/liquid/token.ts
@@ -5,34 +5,10 @@ export type Token = {
 
 export type TokenType = 'text' | 'hardbreak' | 'liquid' | 'variable';
 
-export type TokenSubType =
-  | If
-  | Else
-  | EndIf
-  | Function
-  | Filter
-  | Variable
-  | ForInLoop
-  | EndForInLoop
-  | Newline
-  | Include
-  | ListTabs
-  | EndListTabs
-  | Attributes
-  | Literal;
+export type TokenSubType = Function | Filter | Variable | Attributes | Literal;
 
 export type Literal = 'Literal';
-export type Space = 'Space';
-export type Newline = 'Newline';
-export type If = 'If';
-export type Else = 'Else';
-export type EndIf = 'EndIf';
 export type Function = 'Function';
 export type Filter = 'Filter';
 export type Variable = 'Variable';
-export type ForInLoop = 'ForInLoop';
-export type EndForInLoop = 'EndForInLoop';
-export type Include = 'Include';
-export type ListTabs = 'ListTabs';
-export type EndListTabs = 'EndListTabs';
 export type Attributes = 'Attributes';
